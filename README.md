10c02f7da8aeb134df1f1f2fa22d2cc11ea9eb25
Justice A. Acheampong <iroyal@Justices-MBP.attlocal.net>
2022-02-22 22:11:13 -0500

    changes to README

--- README.md
+++ README.md
@@ -33,4 +33,4 @@ Number Guessor game
    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, or distribute copies of the Software.
 
  ## Acknowledgment
-   Thank you note to my teammates to debug my code; Juman, Tony, Marcus and Matthew.
+   Thank you note to my teammates for helping debug my code; Juman, Tony, Marcus and Matthew.
--- game.js
+++ game.js
@@ -1,22 +1,24 @@
-//generate random for correct answer
+//generate random number for correct answer
 const correct = Math.floor(Math.random() * 99) + 1; 
 
+//input user guess number to match the randomly generated number
 let txtGuess = document.querySelector('#txtGuess');
 console.log(correct)
 
 function checkGuess() {
   
-    //user input guess number value
+    //runs user input guess number value
     if(txtGuess.value < correct) {
         document.querySelector("#response").innerHTML = `Too Low ${txtGuess.value}. Try Again`;
     } else if (txtGuess.value > correct) {
-        document.querySelector("#response").innerHTML = `Too High ${txtGuess.value}.Try Again`;
+        document.querySelector("#response").innerHTML = `Too High ${txtGuess.value}. Try Again`;
     } else if (txtGuess.value == correct) {
-        document.querySelector("#response").innerHTML = "Yay! Correct.  You Smartie Pants!";
+        document.querySelector("#response").innerHTML = "Yay! Correct. You Smartie Pants!";
     } 
     
 }
-//evenListener will be the button click; first parameter is 'click.' and the second parameter is the name of the function to run when the user clicks the button//
+//evenListener will be the button click; first parameter is 'click.' and the second parameter is
+// the name of the function to run when the user clicks the button
 document.querySelector("#clickAgain").addEventListener('click', checkGuess)
 
 
